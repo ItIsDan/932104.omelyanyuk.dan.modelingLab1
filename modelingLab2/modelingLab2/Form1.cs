188ebc50e42c16131dcf8fcd7a717de96ec5709b
Dan Omelyanyuk <bass.chaleng@gmail.com>
2024-02-23 17:27:10 +0700

    Lab2

---
+++ modelingLab2/modelingLab2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modelingLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        double rateEuro, rateDollar; 
+        const double k = 0.1; 
+        Random random = new Random();
+        bool help = true; 
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rateEuro = rateEuro * (1 + k * (random.NextDouble() - 0.5)); 
+            rateDollar = rateDollar * (1 + k * (random.NextDouble() - 0.5)); 
+            chart1.Series[0].Points.AddXY(0, rateDollar);
+            chart1.Series[1].Points.AddXY(0, rateEuro);
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear(); 
+            chart1.Series[1].Points.Clear();
+            help = true; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Legends.Add("euro");
+            chart1.Series.Add("dollar");
+            chart1.Legends.Add("dollar");
+            chart1.Series.Add("euro");
+            for (int i = 0; i < 2; i++)
+            {
+                chart1.Series[i].IsValueShownAsLabel = true;
+                chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[i].BorderWidth = 3;
+                chart1.Series[i].SmartLabelStyle.CalloutLineAnchorCapStyle = System.Windows.Forms.DataVisualization.Charting.LineAnchorCapStyle.None;
+                chart1.Series[i].SmartLabelStyle.CalloutLineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            }
+            
+        }
+
+        private void init_Click(object sender, EventArgs e)
+        {
+            if (help) 
+            {
+                rateEuro = (double)euroSP.Value; 
+                rateDollar = (double)dollarSP.Value; 
+                chart1.Series[0].Points.Clear(); 
+                chart1.Series[1].Points.Clear(); 
+                chart1.Series[0].Points.AddXY(0, rateDollar);
+                chart1.Series[1].Points.AddXY(0, rateEuro); 
+            }
+            help = false;
+
+            if (initButton.Text == "Старт")
+            {
+                initButton.Text = "Стоп";
+                timer1.Start(); 
+            }
+            else
+            {
+                initButton.Text = "Старт";
+                timer1.Stop();
+            }
+        }
+    }
+}
