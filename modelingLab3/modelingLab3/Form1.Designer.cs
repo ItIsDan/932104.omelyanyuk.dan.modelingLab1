824ec6b89b0b992095b2c1bf5002f0aeb62661cc
Dan Omelyanyuk <bass.chaleng@gmail.com>
2024-03-09 02:21:25 +0700

    L3

--- modelingLab3/modelingLab3/Form1.Designer.cs
+++ modelingLab3/modelingLab3/Form1.Designer.cs
@@ -29,12 +29,124 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ruleSP = new System.Windows.Forms.NumericUpDown();
+            this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ruleSP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 459);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rule";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ruleSP
+            // 
+            this.ruleSP.Location = new System.Drawing.Point(67, 457);
+            this.ruleSP.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.ruleSP.Name = "ruleSP";
+            this.ruleSP.Size = new System.Drawing.Size(49, 20);
+            this.ruleSP.TabIndex = 1;
+            this.ruleSP.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.ruleSP.ValueChanged += new System.EventHandler(this.ruleSP_ValueChanged);
+            // 
+            // tableLayout
+            // 
+            this.tableLayout.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayout.ColumnCount = 15;
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayout.Location = new System.Drawing.Point(12, 12);
+            this.tableLayout.Name = "tableLayout";
+            this.tableLayout.RowCount = 13;
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46.37681F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53.62319F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayout.Size = new System.Drawing.Size(605, 425);
+            this.tableLayout.TabIndex = 0;
+            this.tableLayout.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.tableLayout_CellPaint);
+            this.tableLayout.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayout_Paint);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(122, 457);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(59, 20);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(646, 489);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.tableLayout);
+            this.Controls.Add(this.ruleSP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ruleSP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown ruleSP;
+        private System.Windows.Forms.TableLayoutPanel tableLayout;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- modelingLab3/modelingLab3/Form1.cs
+++ modelingLab3/modelingLab3/Form1.cs
@@ -12,9 +12,147 @@ namespace modelingLab3
 {
     public partial class Form1 : Form
     {
+        string rule;
+        List<int> FirstLine;
+        Color[,] colors = new Color[15, 14];
+        List<int> line = new List<int>(new int[15]);
+        List<int> otherLine = new List<int>(new int[15]);
+
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 15; i++)
+                for (int j = 0; j < 14; j++)
+                    colors[i, j] = SystemColors.Control;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool activeFirstTime = true;
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            tableLayout.Refresh();
+            rule = Convert.ToString((int)ruleSP.Value, 2); // Конвертация числа в основание двойки
+            if (rule.Length != 8)
+            {
+                int len = 8 - rule.Length;
+                for (int i = 0; i < len; i++)
+                    rule = rule.Insert(0, "0");
+            }
+
+            if (activeFirstTime)
+            {
+                FirstLine = new List<int>(new int[15]);
+                for (int i = 0; i < FirstLine.Count; i++)
+                    FirstLine[i] = 0;
+
+                    FirstLine[7] = 1;
+                    FirstLine[8] = 1;
+                    FirstLine[12] = 1;
+                    colors[7, 0] = Color.Black;
+                    colors[8, 0] = Color.Black;
+                    colors[12, 0] = Color.Black;
+
+                for (int i = 0; i < otherLine.Count; i++)
+                    otherLine[i] = 0;
+
+                tableLayout.Refresh();
+                line = FirstLine;
+            }
+            activeFirstTime = false;
+
+            if (startButton.Text == "Start")
+            {
+                startButton.Text = "Stop";
+                timer1.Start();
+            } 
+            else 
+            {
+                startButton.Text = "Start";
+                timer1.Stop();
+            }
+        }
+        int Count = 1;
+
+        public int check(string help)
+        {
+            switch (help)
+            {
+                case "110":
+                    return rule[1] - 48;
+                case "101":
+                    return rule[2] - 48;
+                case "011":
+                    return rule[4] - 48;
+                case "010":
+                    return rule[5] - 48;
+                case "100":
+                    return rule[3] - 48;
+                case "000":
+                    return rule[7] - 48;
+                case "111":
+                    return rule[0] - 48;
+                case "001":
+                    return rule[6] - 48;
+                default:
+                    return rule[0] - 48;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           for (int i = 0; i < otherLine.Count; i++)
+                 line[i] = otherLine[i];
+
+            for (int i = 0; i < line.Count; i++)
+            {
+                if (i != 0 && i != line.Count - 1)
+                {
+                    string Result = "";
+                    Result = line[i - 1].ToString() + line[i].ToString() + line[i + 1].ToString();
+                    int result = check(Result);
+                    otherLine[i] = result;
+                    
+                    if (otherLine[i] == 0)
+                    {
+                        colors[i, Count] = Color.Green;
+                    }
+                }
+            }
+
+            tableLayout.Refresh();
+            Count++;
+
+            if (Count == 13)
+            {
+                timer1.Stop();
+                startButton.Enabled = false;
+            }
+        }
+
+        private void tableLayout_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ruleSP_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayout_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            if (colors != null)
+            {
+                SolidBrush brush = new SolidBrush(colors[e.Column, e.Row]);
+                e.Graphics.FillRectangle(brush, e.CellBounds);
+            }
         }
     }
 }
