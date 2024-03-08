ca2fc6bdf6ab1d94ebcf070527b6d41ea0f25b65
Dan Omelyanyuk <bass.chaleng@gmail.com>
2024-03-09 06:00:05 +0700

    Added draw event L4

--- modelingLab4/modelingLab4/Form1.Designer.cs
+++ modelingLab4/modelingLab4/Form1.Designer.cs
@@ -29,12 +29,223 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.activate = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.scaleSP = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleSP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(465, 452);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Новая карта";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // activate
+            // 
+            this.activate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.activate.Location = new System.Drawing.Point(383, 503);
+            this.activate.Name = "activate";
+            this.activate.Size = new System.Drawing.Size(94, 21);
+            this.activate.TabIndex = 2;
+            this.activate.Text = "Запустить";
+            this.activate.UseVisualStyleBackColor = true;
+            this.activate.Click += new System.EventHandler(this.activate_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(310, 506);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(48, 20);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.Text = "3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(310, 476);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 20);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.Text = "3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 506);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 20);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 476);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 20);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(170, 479);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 14);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Минимум для появления";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(170, 509);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 14);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Максимум для появления";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 509);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 14);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Максимум соседей";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 479);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 14);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Минимум соседей";
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.clear.Location = new System.Drawing.Point(383, 533);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(94, 21);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 544);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Масштаб";
+            // 
+            // scaleSP
+            // 
+            this.scaleSP.Location = new System.Drawing.Point(72, 542);
+            this.scaleSP.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.scaleSP.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.scaleSP.Name = "scaleSP";
+            this.scaleSP.Size = new System.Drawing.Size(62, 20);
+            this.scaleSP.TabIndex = 21;
+            this.scaleSP.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.scaleSP.ValueChanged += new System.EventHandler(this.scaleSP_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(489, 569);
+            this.Controls.Add(this.scaleSP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.activate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleSP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button activate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown scaleSP;
     }
 }
 
--- modelingLab4/modelingLab4/Form1.cs
+++ modelingLab4/modelingLab4/Form1.cs
@@ -12,9 +12,105 @@ namespace modelingLab4
 {
     public partial class Form1 : Form
     {
+        bool[,] map;
+
+        Bitmap bitmap;
+        Graphics graphics;
+        Random rand = new Random();
+        Pen pen = new Pen(Color.Black, 1);
+
+        int m;
+        int n;
+
         public Form1()
         {
             InitializeComponent();
+            m = (int)pictureBox1.Width - 1 / 4;
+            n = (int)pictureBox1.Height - 1 / 4;
+            map = new bool[m, n];
+        }
+
+        void generateMap()
+        {
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    map[i, j] = Convert.ToBoolean(rand.Next(0, 2));
+        }
+
+        void drawMap()
+        {
+            var scale = (pictureBox1.Width / pictureBox1.Height) * (int)scaleSP.Value;
+
+            graphics.Clear(Color.White);
+
+            for (int i = 0; i < pictureBox1.Width / 4; i++)
+            {
+                for (int j = 0; j < pictureBox1.Height / 4; j++)
+                { 
+                    if (map[i, j])
+                    {
+                        graphics.DrawRectangle(pen, i * scale, j * scale, scale, scale);
+                        graphics.FillRectangle(new SolidBrush(Color.Black), i * scale, j * scale, scale, scale);
+                    }
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    map[i, j] = false;
+            drawMap();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generateMap();
+            drawMap();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            graphics = Graphics.FromImage(bitmap);
+        }
+
+        private void activate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scaleSP_ValueChanged(object sender, EventArgs e)
+        {
+            drawMap();
         }
     }
 }
